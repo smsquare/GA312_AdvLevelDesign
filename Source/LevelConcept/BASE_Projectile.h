a6790bf9ea552580e15c21145139ac9c1265168a
Steven Maartmann-Moe <smsquare87@gmail.com>
2017-10-15 16:37:13 -0700

    Created BASE_Projectile .h / .cpp files from Editor. Actual implementation of features to come.

---
+++ Source/LevelConcept/BASE_Projectile.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "BASE_Projectile.generated.h"
+
+UCLASS()
+class LEVELCONCEPT_API ABASE_Projectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABASE_Projectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
