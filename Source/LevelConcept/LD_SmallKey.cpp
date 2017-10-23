6644194b746ce26e57b238eb5be08d309bee83f7
Steven Maartmann-Moe <smsquare87@gmail.com>
2017-10-23 01:45:05 -0700

    Player can pickup keys and add them to inventory and UI.

--- Source/LevelConcept/LD_SmallKey.cpp
+++ Source/LevelConcept/LD_SmallKey.cpp
@@ -15,6 +15,7 @@ ALD_SmallKey::ALD_SmallKey() {
 
 	SmallKeyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Small Key Mesh"));
 	RootComponent = SmallKeyMesh;
+	SmallKeyMesh->SetCollisionProfileName(FName("PickupObject"));
 	SmallKeyMesh->OnComponentBeginOverlap.AddDynamic(this, &ALD_SmallKey::PickupDetection);
 }
 
@@ -34,7 +35,9 @@ void ALD_SmallKey::Tick(float DeltaTime) {
 void ALD_SmallKey::PickupDetection(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult) {
 	ALD_Player* player = Cast<ALD_Player>(OtherActor);
 	if (player) {
-		//TODO player->PickupSmallKey();
+		if (KeyColor != EKeyColor::KC_INVALID) {
+			player->KeyRing.PickUpKey(KeyColor);
+		}		
 		Destroy();
 	}
 }
