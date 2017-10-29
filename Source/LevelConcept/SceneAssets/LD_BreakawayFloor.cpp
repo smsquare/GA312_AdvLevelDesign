1d086a5da2a12a433704a28a78f5c3fce1596c6f
Steven Maartmann-Moe <smsquare87@gmail.com>
2017-10-29 16:17:03 -0700

    Setup initial consturctor with default floor mesh. - Relocated static floor meshes to new folder for better organization.

--- Source/LevelConcept/SceneAssets/LD_BreakawayFloor.cpp
+++ Source/LevelConcept/SceneAssets/LD_BreakawayFloor.cpp
@@ -6,20 +6,36 @@
 
 // Sets default values
 ALD_BreakawayFloor::ALD_BreakawayFloor() {
-	//TODO: IMPLEMENT CONSTRUCTOR HELPERS
-	/// One time init over helpers for loading default meshes or other assets...
-	//struct FConstructorStatics {
-	//
-	//};
-	//
+	// One time init over helpers for loading default meshes or other assets...
+	struct FBreakawayStatics {
+		ConstructorHelpers::FObjectFinderOptional<UStaticMesh> defaultFloorMesh;
+		FBreakawayStatics() :
+			defaultFloorMesh(TEXT("/Game/Geometry/Meshes/Floors/SM_Floor_500x1000.SM_Floor_500x1000")) {
+		}
+	};
+	static FBreakawayStatics BreakawayStatics;
 
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ 	// Start with tick turned off.
 	PrimaryActorTick.bCanEverTick = true;
-	
+	PrimaryActorTick.bStartWithTickEnabled = false;
 	// init Settings
+	FloorSize = EFloorSize::FS_500;
 	TimeToBreakaway = 1.25;
 	TimeToRespawn	= 3.0;
 	RespawnLocation = GetActorLocation();
+
+	// Setup floor mesh component
+	FloorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Floor Mesh"));
+	FloorMesh->SetStaticMesh(BreakawayStatics.defaultFloorMesh.Get());
+	RootComponent = FloorMesh;
+
+	// Setup floor collider;
+	FloorCollider = CreateDefaultSubobject<UBoxComponent>(TEXT("Floor Collider"));
+	FloorCollider->AttachToComponent(FloorMesh, FAttachmentTransformRules::KeepRelativeTransform);
+}
+
+void ALD_BreakawayFloor::SetColliderExtents(EFloorSize) {
+	
 }
 
 // Called when the game starts or when spawned
--- Source/LevelConcept/SceneAssets/LD_BreakawayFloor.h
+++ Source/LevelConcept/SceneAssets/LD_BreakawayFloor.h
@@ -3,13 +3,31 @@
 #include "GameFramework/Actor.h"
 #include "LD_BreakawayFloor.generated.h"
 
+/***********
+	ENUMS
+*************/
+UENUM(BlueprintType)
+enum class EFloorSize : uint8 {
+	FS_100 = 0		UMETA(DisplayName = "100"),
+	FS_250			UMETA(DisplayName = "250"),
+	FS_500			UMETA(DisplayName = "500"),
+	FS_INVALID = 99	UMETA(Hidden)
+};
+
+//********************************************************* //
+//						BREAKAWAY FLOOR						//
+//**********************************************************//
 UCLASS()
 class LEVELCONCEPT_API ALD_BreakawayFloor : public AActor {
 	GENERATED_BODY()
+
 /********************** 
 		VARIABLES 
 *************************/
 public:
+	// The width of the floor tile
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings")
+	EFloorSize FloorSize;
 	// The time it takes for the platform to breakaway, 
 	// i.e. the amount of time the player can stand on before they fall.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings")
@@ -19,12 +37,15 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Settings")
 	float TimeToRespawn;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings|Collider")
+	UBoxComponent* FloorCollider;
+
 	// The mesh used for the floor.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
 	UStaticMeshComponent* FloorMesh;
 
-private: 
 
+private: 
 	// Location the platform will appear at on respawn. This is the starting location.
 	UPROPERTY(VisibleAnywhere, Category = "Settings")
 	FVector RespawnLocation;
@@ -34,6 +55,7 @@ private:
 *************************/
 public:	
 	ALD_BreakawayFloor();
+	void SetColliderExtents(EFloorSize);
 protected:
 	virtual void BeginPlay() override;
 public:	
