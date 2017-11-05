9eb7b6fbc9927abea40f9bf02bad7ce447fb053b
Steven Maartmann-Moe <smsquare87@gmail.com>
2017-11-04 18:23:57 -0700

    Added initial WeaponPickup files.

---
+++ Source/LevelConcept/Pickups/LD_WeaponPickup.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "BASE/BASE_ItemPickup.h"
+#include "LD_WeaponPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEVELCONCEPT_API ALD_WeaponPickup : public ABASE_ItemPickup
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
