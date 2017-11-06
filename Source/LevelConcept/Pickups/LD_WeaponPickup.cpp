3f221166a1fea3eaf20d3e4db625e7170ba48795
Steven Maartmann-Moe <smsquare87@gmail.com>
2017-11-06 14:02:37 -0800

    Override PickupDetection to handle WeaponPickup. - Added TypeofWeapon to WeaponPickup.

--- Source/LevelConcept/Pickups/LD_WeaponPickup.cpp
+++ Source/LevelConcept/Pickups/LD_WeaponPickup.cpp
@@ -1,9 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "LevelConcept.h"
+#include "LD_Player.h"
 #include "LD_WeaponPickup.h"
 
 ALD_WeaponPickup::ALD_WeaponPickup() {
 	PickupType = EPickupType::PT_WEAPON;
 	PickupName = "Default Weapon Pickup";
+	TypeOfWeapon = EWeaponType::WT_INVALID;
+}
+
+void ALD_WeaponPickup::PickupDetection(UPrimitiveComponent* OverlappedComponent, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
+	if (!HasBeenPickedUp) {
+		ALD_Player* player = Cast<ALD_Player>(OtherActor);
+		if (player) {
+			if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Purple, "WeaponPickupDetection");
+			HasBeenPickedUp = true;
+			PickupMesh->SetVisibility(false);
+			// player->PickupWeapon(TypeOfWeapon);
+		}
+	}
 }
--- Source/LevelConcept/Pickups/LD_WeaponPickup.h
+++ Source/LevelConcept/Pickups/LD_WeaponPickup.h
@@ -1,13 +1,17 @@
 #pragma once
 
 #include "BASE/BASE_ItemPickup.h"
+#include "LD_Weapon.h"
 #include "LD_WeaponPickup.generated.h"
 
 UCLASS()
 class LEVELCONCEPT_API ALD_WeaponPickup : public ABASE_ItemPickup {
 	GENERATED_BODY()
-
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Info")
+	EWeaponType TypeOfWeapon;
 public:
 	ALD_WeaponPickup();	
-	
+
+	void PickupDetection(UPrimitiveComponent* OverlappedComponent, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 };
