7e5eeb81d97046f49dcc1b85d6195ebbfe53cdb0
Steven Maartmann-Moe <smsquare87@gmail.com>
2017-11-06 23:58:37 -0800

    Added CurrentProjectile, Listof_Projectiles, Listof_MaxAmmo - CurrentProjectile stores the blueprint info for the equiped projectile. - ListofProjectiles stores the blueprint references to all weapons. - ListofMaxAmmo stores all the maxAmmo values for each weapon type.

--- Source/LevelConcept/Weapons/LD_Weapon.cpp
+++ Source/LevelConcept/Weapons/LD_Weapon.cpp
@@ -1,11 +1,27 @@
 #include "LevelConcept.h"
 #include "LD_Weapon.h"
 
+/*****************************************************
+**													**
+**					FWeaponStats					**
+**													**
+*****************************************************/
 FWeaponStats::FWeaponStats() {
-	CurrAmmo = 0;
-	MaxAmmo = 0;
+	CurrAmmo = -1;
+	MaxAmmo = -1;
+	// MaxAmmo Reference
+	ListOf_MaxAmmo[(int)EWT::WT_DEFAULT] = -1;
+	ListOf_MaxAmmo[(int)EWT::WT_LASER] = 15;
+	ListOf_MaxAmmo[(int)EWT::WT_GRENADE] = 5;
+
+	ListOf_Projectiles.AddDefaulted(3);
 }
 
+/*****************************************************
+**													**
+**					  FWeapon						**
+**													**
+*****************************************************/
 FWeapon::FWeapon() {
 	CurrentWeapon = EWeaponType::WT_DEFAULT;
 }
